62257fff0beece4fd4aedd0b077bc41208acf56f
Jerin <jerinji2016@gmail.com>
2024-01-17 03:02:09 +0530

    flutter cleaner script

---
+++ flutter_cleaner/README.md
@@ -0,0 +1,17 @@
+# flutter_cleaner
+
+Script to clean all flutter project builds
+
+## __Why Bother?__  
+
+Mostly we dont clear flutter builds resulting in consuming GBs of space.
+
+***
+
+### Good to follow
+
+Have a habit of grouping all project of under a single folder, something like `Projects` or `Works`.
+
+### Working with script
+
+Change the `FOLDER_PATH` within the [script](./flutter_cleaner.sh) to your parent folder containing all Flutter Projects.
---
+++ flutter_cleaner/flutter_cleaner.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Replace /path/to/your/folder with the path to the specific folder
+FOLDER_PATH="/Users/jerin/Documents/Projects"
+
+# Initialize a counter
+COUNT=0
+
+echo "🔍 Searching Flutter Projects in $FOLDER_PATH"
+
+# Save the current directory
+CURRENT_DIR=$(pwd)
+
+# Find each pubspec.yaml file and run flutter clean in its directory
+find "$FOLDER_PATH" -name 'pubspec.yaml' | while read file; do
+    COUNT=$((COUNT+1))
+    DIR=$(dirname "$file")
+    echo "🧹 $COUNT) Running 'flutter clean' in $DIR"
+    cd "$DIR"
+    flutter clean
+    cd "$CURRENT_DIR"
+    echo -e "-----------------\n"
+done
+
+# Print the count
+echo "✅ Found $COUNT Flutter Projects!"
